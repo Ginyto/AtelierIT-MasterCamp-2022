d529445fd70e4fc448d19618196ad32bd3b145eb
Ginyto <agianlucca@gmail.com>
2022-06-10 10:04:27 +0200

    debut de la classe course

--- course.java
+++ course.java
@@ -4,5 +4,28 @@ public class Course {
 
     Cycliste[] cyclistes;
 
+
+    public Course(String nom){
+        this.setNom(nom);
+    }
+
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Cycliste[] getCyclistes() {
+        return this.cyclistes;
+    }
+
+    public void setCyclistes(Cycliste[] cyclistes) {
+        this.cyclistes = cyclistes;
     
+
+
+
 }
