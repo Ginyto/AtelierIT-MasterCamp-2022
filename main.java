e45bd669a00e35a8477eb6f7054efba149c8f4a8
Ginyto <agianlucca@gmail.com>
2022-06-10 10:29:05 +0200

    test

--- main.java
+++ main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello there");
+
+        System.out.println("Hello there\nPlease enter the name of the race\nRace : ");
+
+        Scanner saisie = new Scanner(System.in);
+
+        Course course = new Course(saisie.nextLine());
+
+        
+
+
+
+
     }
 }
